1b7e59daa567b5e78ca921df0e0869151858ab02
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-09 03:19:59 -0500

    Fix linters error with bundle exec rubocop

--- morse_code.rb
+++ morse_code.rb
@@ -35,40 +35,38 @@ MORSE_CODE = {
   '---..' => '8',
   '----.' => '9',
   '-----' => '0'
-}
+}.freeze
 
 def decode_char(char)
   (MORSE_CODE[char])
 end
 
-print ("1-"+decode_char(".-")) #A
-print ("\n")
-print "2-"+decode_char("-----") #0
-print ("\n")
+print("1-#{decode_char('.-')}") # A
+print("\n")
+print "2-#{decode_char('-----')}" # 0
+print("\n")
 
-def decode_word (word_in_morse)
-    word_arr = []
-    word_in_morse.split.each {
-      |ele|
-      word_arr.push(decode_char(ele))
-    }
-    return word_arr.join
-end    
-    
-p "3-"+decode_word('-- -.-- -----') 
+def decode_word(word_in_morse)
+  word_arr = []
+  word_in_morse.split.each do |ele|
+    word_arr.push(decode_char(ele))
+  end
+  word_arr.join
+end
+
+p "3-#{decode_word('-- -.-- -----')}"
 # "MY0"
 
-def decode (message)
+def decode(message)
   arr_msg = []
-  message.split("   ").each {
-    |word|
+  message.split('   ').each do |word|
     # array of words ex: ["MY", "NAME", "IS"]
     arr_msg.push(decode_word(word))
-  }
-  arr_msg.join(" ")
-end  
+  end
+  arr_msg.join(' ')
+end
 
-p "4-"+decode("-- -.--   -. .- -- .   .. ...")
+p "4-#{decode('-- -.--   -. .- -- .   .. ...')}"
 # "MY NAME IS"
-p "5-"+decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+p "5-#{decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')}"
 # "A BOX FULL OF RUBIES"
